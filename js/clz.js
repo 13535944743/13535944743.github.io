03b2ec171a738556b9107533c011d47fa4d5cc66
clz <1926389897@qq.com>
2022-03-27 16:05:04 +0800

    Site updated: 2022-03-27 16:05:02

--- js/clz.js
+++ js/clz.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   let clzPjax = $(document).pjax('a[target!=_blank]', '#pageContent', { fragment: '#pageContent', timeout: 6000, scroll: false });
   clzPjax.on('pjax:complete', function () {
+    console.log(123)
+    $.getScript('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
     // $.getScript('/libs/valine/av-min.js');
     // $.getScript('https://cdn.jsdelivr.net/gh/HCLonely/Valine@latest/dist/Valine.min.js');
 
