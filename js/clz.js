3934d35628d287182f4d2539405bc1de035526a4
clz <1926389897@qq.com>
2022-06-29 23:47:05 +0800

    Site updated: 2022-06-29 23:47:03

--- js/clz.js
+++ js/clz.js
@@ -1,9 +1,10 @@
 window.onload = function () {
   let clzPjax = $(document).pjax('a[target!=_blank]', '#pageContent', { fragment: '#pageContent', timeout: 6000, scroll: false });
   clzPjax.on('pjax:complete', function () {
-    console.warn(123)
     if ($('#toc-content').length) {
-      tocbot.destroy()
+      if (tocbot) {
+        tocbot.destroy()
+      }
     }
   })
 }
