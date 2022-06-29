9040ac75d5fb54c4af986ec81ad412c8a0c740f2
clz <1926389897@qq.com>
2022-06-29 22:11:46 +0800

    Site updated: 2022-06-29 22:11:44

--- js/clz.js
+++ js/clz.js
@@ -1,19 +1,7 @@
 window.onload = function () {
   let clzPjax = $(document).pjax('a[target!=_blank]', '#pageContent', { fragment: '#pageContent', timeout: 6000, scroll: false });
   clzPjax.on('pjax:complete', function () {
-    // console.log(123)
-    // $.getScript('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js')
-    // $.getScript('/libs/valine/av-min.js');
-    // $.getScript('https://cdn.jsdelivr.net/gh/HCLonely/Valine@latest/dist/Valine.min.js');
-
-
-    // $.getScript('/libs/tocbot/tocbot.min.js');
-    // let temp = $.getScript('//cdn.bootcss.com/canvas-nest.js/1.0.0/canvas-nest.min.js');
-    // console.log(temp);
-
-  })
-
-  clzPjax.on('pjax:send', function () {
+    console.warn(123)
     if ($('#toc-content').length) {
       tocbot.destroy()
     }
@@ -21,8 +9,3 @@ window.onload = function () {
 }
 
 
-
-// $(document).on('ready pjax:beforeReplace', async function (event) {
-//   console.log(window.location.pathname);
-//   valine.setPath(window.location.pathname);
-// });
